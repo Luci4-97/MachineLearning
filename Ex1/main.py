dd80c545f7c46eeee7f10bc1b685e2dc481cd0b7
Setsuna <295895691@qq.com>
2018-05-19 15:46:40 +0800

    四分类

--- Ex1/main.py
+++ Ex1/main.py
@@ -1,49 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
 class import_data(object):
+    """
+    导入数据、编码、归一化
+    """
+
     def __init__(self):
-        self.name = np.loadtxt("data.csv", dtype=np.str, usecols=(1,), delimiter=',')
-        self.place = np.loadtxt("data.csv", dtype=np.int32, usecols=(4,), delimiter=',')
+        self.names = np.loadtxt("data.csv", dtype=np.str, usecols=(1,), delimiter=',')
+        self.place = np.loadtxt("data.csv", dtype=np.float32, usecols=(4,), delimiter=',')
         self.__init_X__()
         self.__init_y__()
         pass
 
-    def __encode__(self, X):
-        if len(X) == 0:
+    def __encode__(self, names, n_max=8):
+        """
+        给姓名编码
+        :param names: 姓名列表
+        :param n_max: 姓名最长长度
+        :return: 编码后的矩阵
+        """
+        if len(names) == 0:
             return
-        X_encode = []
-        for x in X:
-            sum = 0
-            for c in x:
-                sum = sum * 10 + ord(c)
-            X_encode.append(sum)
-        X_encode = np.array(X_encode, dtype=np.int32)
-        return X_encode
+        names_mat = []
+        for name in names:
+            name_vec = []
+            for i in range(n_max):
+                try:
+                    x = (ord(name[i]) - 96) / 26
+                except:
+                    x = 0
+                name_vec.append(x)
+            names_mat.append(name_vec)
+        names_mat = np.array(names_mat, dtype=np.float32)
+        return names_mat
 
     def __init_X__(self):
-        name = self.__encode__(self.name).reshape((-1, 1))
-        place = self.place.reshape((-1, 1))
+        """
+        初始化样本
+        :return: 样本
+        """
+        name = self.__encode__(self.names)
+        place = (self.place.reshape((-1, 1)) - 101) / 108
         self.X = np.concatenate((name, place), axis=1)
 
     def __init_y__(self):
+        """
+        初始化类别标签
+        :return: 列别标签
+        """
         scores = np.loadtxt("data.csv", dtype=np.float32, usecols=(8,), delimiter=',')
         y = []
         for score in scores:
             if score > 90:
-                y.append('b')
+                # y.append('b')
+                y.append(0)
             else:
                 if score > 80:
-                    y.append('c')
+                    # y.append('c')
+                    y.append(1)
                 else:
                     if score > 70:
-                        y.append('g')
+                        # y.append('g')
+                        y.append(2)
                     else:
                         if score > 60:
-                            y.append('k')
+                            # y.append('k')
+                            y.append(3)
                         else:
-                            y.append('m')
+                            # y.append('m')
+                            y.append(4)
         self.y = np.array(y)
 
 
@@ -51,5 +79,7 @@ if __name__ == '__main__':
     data = import_data()
     X = data.X
     y = data.y
+    lda = LinearDiscriminantAnalysis(n_components=2)
+    lda.fit(X, y)
+    X = lda.transform(X)
     plt.scatter(X[:, 0], X[:, 1], c=y, marker='.')
-    plt.show()
